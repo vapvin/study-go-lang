c210648aaf6594b18c446117318222315773bd1c
vapvin <tpehdj12@daum.net>
2020-06-29 16:29:26 +0900

    Umm..

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
-
-import (
-	"fmt"
-)
-
+import "fmt"
 func main() {
-	fmt.Println("|\\_/|")
-	fmt.Println("|q p|   /}")
-	fmt.Println("( 0 )\"\"\"\\")
-	fmt.Println("|\"^\"`    |")
-	fmt.Println("||_/=\\\\__|")
+	var a, b, c, d, e, f int
+	fmt.Scanf("%d %d %d\n", &a, &b, &c)
+	fmt.Scanf("%d %d %d\n", &d, &e, &f)
+	firLine := (a*100)+(b*10)+(c*1)
+	secLine := (d*100)+(e*10)+(f*1)
+	fmt.Printf("%d\n", firLine * f)
+	fmt.Printf("%d\n", firLine * e)
+	fmt.Printf("%d\n", firLine * d)
+	fmt.Printf("%d\n", firLine * secLine)
 }
