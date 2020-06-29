8853673b0b208b4de8564f3342b82b556463c9e6
vapvin <tpehdj12@daum.net>
2020-06-29 16:02:01 +0900

    Dog Printings

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 )
 
 func main() {
-	fmt.Println("\\    /\\")
-	fmt.Println(" )  ( ')")
-	fmt.Println("(  /  )")
-	fmt.Println(" \\(__)|")
+	fmt.Println("|\\_/|")
+	fmt.Println("|q p|   /}")
+	fmt.Println("( 0 )\"\"\"\\")
+	fmt.Println("|\"^\"`    |")
+	fmt.Println("||_/=\\\\__|")
 }
